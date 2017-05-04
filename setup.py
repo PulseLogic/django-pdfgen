45693864514815c76ea90b83a09bec82f2666cae
Jeroen Op 't Eynde <jeroen@vikingco.com>
2017-05-04 16:31:00 +0200

    Prep for pushing to Pypi

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ import pdfgen
 
 
 setup(
-    name="django-pdfgen",
+    name='django-pdfgen',
     version=pdfgen.__version__,
-    url='https://github.com/citylive/django-pdfgen',
+    url='https://github.com/vikingco/django-pdfgen',
     license='BSD',
-    description="Generation of PDF documents",
+    description='Generation of PDF documents',
     long_description=open('README.rst', 'r').read(),
-    author='Jef Geskens, City Live nv',
+    author='Jef Geskens, Unleashed NV',
+    author_email='operations@unleashed.be',
     packages=find_packages('.'),
-    #package_dir={'': 'templates/*'},
-    install_requires="reportlab>=3.2.0",
+    zip_safe=False,
+    install_requires='reportlab>=3.2.0',
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
